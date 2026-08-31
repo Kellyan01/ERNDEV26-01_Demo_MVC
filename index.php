45b9bb7ff010af364f75aefa62f6854b347ce917
Kellyan01 <depriester.yoann@neuf.fr>
2026-08-31 11:18:09 +0200

    Correction Exercice 13 et 14 : page Moncompte + lien déconnexion + Affichage Dynamique menu navigation

--- index.php
+++ index.php
@@ -14,10 +14,12 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Controller\ControllerUser as MonUser; // avec AS je fourni un alias au nom de ma classe
 use Controller\ControllerArticle;
+use Controller\ControllerAccount;
 use Model\ModelUser;
 use Model\ModelArticle;
 use View\ViewUser;
 use View\ViewArticle;
+use View\ViewAccount;
 use Utils\Utils;
 
 //1. Récupérer l'url demandé par l'utilisateur
@@ -37,10 +39,27 @@ switch ($path) {
         //Rendu de l'affichage
         $controller->render();
         break;
+
     case $_ENV['articles'] :
         $controller = new ControllerArticle(new ModelArticle(Utils::connect()), new ViewArticle("Articles","./public/src/script/scriptArticle.js"));
         $controller->render();
         break;
+
+    case $_ENV['moncompte'] :
+        //Affichage de la page
+        $controller = new ControllerAccount(new ModelUser(Utils::connect()), new ViewAccount("Mon Compte",""));
+        $controller->render();
+        break;
+
+    case $_ENV['deconnexion'] :
+        //Destruction de la sessions
+        session_destroy();
+        //Redirection vers la page d'accueil (/MVC/ -> $_ENV['utilisateurs'])
+        //ici location permet de modifier l'url de la requête => cela produit une redirection HTTP
+        header('location:'.$_ENV['utilisateurs']);
+        exit;
+        break;
+
     case $_ENV['test'] : //route de test pour garder les autres propres
         //TEST DE FINDBYEMAIL() DU MODELUSER 
         //1. Créer un objet modelUser
@@ -51,6 +70,7 @@ switch ($path) {
         $data = $model->setEmail('yoannnn@gmail.fr')->findByEmail();
         var_dump($data);
         break;
+        
     default:
         echo "erreur 404";
         break;
